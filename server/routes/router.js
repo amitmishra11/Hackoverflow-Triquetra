d83c70247fd822c06e11b42a84f87c35151b5873
abhishek <lonulobu@gmail.com>
2022-04-08 03:35:45 +0530

    login in branch auth

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const route = express.Router()
 
+const {ensureAuth,ensureGuest}=require('../middleware/auth');
+
 // const services = require('../services/render');
 const controller = require('../controller/controller');
 
-route.get('/', (req,res)=>
+route.get('/',ensureGuest, (req,res)=>
 {
 // res.send("Crud Application by abhishek");
 res.render('index');
+})
+
+route.get('/index2',ensureAuth, (req,res)=>
+{
+// res.send("Crud Application by abhishek");
+res.render('index2',{
+    name:req.user.displayName
+});
 })
 
 route.get('/login', (req,res)=>
@@ -28,7 +38,7 @@ route.get('/blog', (req,res)=>
 res.render('blog');
 })
 
-route.get('/maps', (req,res)=>
+route.get('/maps',ensureAuth, (req,res)=>
 {
 // res.send("Crud Application by abhishek");
 res.render('maps');
@@ -40,10 +50,17 @@ route.get('/medc', (req,res)=>
 res.render('medc');
 })
 
+// route.get('/logout', (req,res)=>
+// {
+// // res.send("Crud Application by abhishek");
+// res.render('index');
+// })
+
 // API
-route.post('/api/users', controller.create);
-route.get('/api/users', controller.find);
-route.put('/api/users/:id', controller.update);
-route.delete('/api/users/:id', controller.delete);
+// route.post('/api/users', controller.create);
+
+// route.get('/api/users', controller.find);
+// route.put('/api/users/:id', controller.update);
+// route.delete('/api/users/:id', controller.delete);
 
 module.exports = route
