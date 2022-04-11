efaeb4c12ed50eea818da566e38c52bb94727dd1
abhishek <lonulobu@gmail.com>
2022-04-11 23:40:50 +0530

    donate

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const route = express.Router()
-
+const Razorpay = require('razorpay');
 const {ensureAuth,ensureGuest}=require('../middleware/auth');
 
 // const services = require('../services/render');
 const controller = require('../controller/controller');
+const router = require('./auth');
+
+const instance=new Razorpay({
+    key_id :"rzp_test_TjfI1Q9set4utL",
+    key_secret:"E49XppgTgpFBSdkAP6XzIbGx"
+})
+
 
 route.get('/',ensureGuest, (req,res)=>
 {
@@ -50,11 +57,49 @@ route.get('/medc', (req,res)=>
 res.render('medc');
 })
 
-// route.get('/logout', (req,res)=>
-// {
-// // res.send("Crud Application by abhishek");
-// res.render('index');
-// })
+route.get('/pay', (req,res)=>
+{
+// res.send("Crud Application by abhishek");
+res.render('pay');
+})
+route.get('/donate', (req,res)=>
+{
+// res.send("Crud Application by abhishek");
+res.render('donate');
+})
+
+route.get('/sux', (req,res)=>
+{
+// res.send("Crud Application by abhishek");
+res.render('sux');
+})
+
+
+
+route.post("/api/payment/order",(req,res)=>{
+	var params = {
+		// amount: document.getElementById('rzp-text').value,  
+		amount: 100000, 
+		currency: "INR",
+		payment_capture: '1'
+	  };
+	
+	// params=req.body;
+	instance.orders.create(params).then((data) => {
+		console.log("created id")
+		res.json(data)
+		//    res.send({"sub":data,"status":"success"});
+	}).catch((error) => {
+		console.log("failed id")
+		   res.send({"sub":error,"status":"failed"});
+	})
+	});
+
+route.get('/logout', (req,res)=>
+{
+// res.send("Crud Application by abhishek");
+res.render('index');
+})
 
 // API
 // route.post('/api/users', controller.create);
